77f2c47aa5c604ddb19576a36f44ac670370547b
Dimitri Kokkonis <dimitri.kokkonis@cea.fr>
2024-07-09 20:08:39 +0200

    Revert "Thttpd: tentative fix to infinite loop"
    Now that we have figured out the logic of thttpd, time to revert to the
    original version.
    
    This reverts commit 7d97c511b8743a39e698f8f593f71f4c12a95d5e.

--- authentic/d-link-1.13A/original/thttpd.c
+++ authentic/d-link-1.13A/original/thttpd.c
@@ -1326,16 +1326,9 @@ handle_newconnect( struct timeval* tvP, int listen_fd )
 	/* Get the connection. */
 	switch ( httpd_get_conn( hs, listen_fd, c->hc ) )
 	    {
-            /* Some error happened.  Run the timers, then the
-            ** existing connections.  Maybe the error will clear.
-            */
-            case GC_FAIL:
-            tmr_run( tvP );
-            return 0;
-
-            /* No more connections to accept for now. */
-            case GC_NO_MORE:
-            return 1;
+	    case GC_FAIL:
+	    case GC_NO_MORE:
+	    return 1;
 	    }
 	c->conn_state = CNST_READING;
 	++numconnects;
